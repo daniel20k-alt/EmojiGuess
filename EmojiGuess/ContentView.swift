f90e8575bb57d3cbd6a54bbd36eb501b1af51364
Testing_author <testing_beta@mail.com>
2021-05-16 12:46:37 +0300

    Configuring the ContentView with the Stacks

--- EmojiGuess/ContentView.swift
+++ EmojiGuess/ContentView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack() {
+            ForEach(0..<4) { index in
+                ZStack() {
+                    RoundedRectangle(cornerRadius: 11.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 11.0).stroke(lineWidth: 4)
+                    Text("🚙")
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(Color.yellow)
+        .font(Font.largeTitle)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
