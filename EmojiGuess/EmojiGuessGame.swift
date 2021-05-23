1acefc658c28960adbdc9835872e637add2627f4
Testing_author <testing_beta@mail.com>
2021-05-23 19:36:22 +0300

    Configuring and incorporating the app's MVVM.

---
+++ EmojiGuess/EmojiGuessGame.swift
@@ -0,0 +1,34 @@
+//
+//  EmojiGuessGame.swift
+//  EmojiGuess
+//
+//  Created by DDDD on 20/05/2021.
+//
+
+//ViewModel
+import SwiftUI
+
+class EmojiGuessGame {
+    
+    private var game: LogicBoard<String> = EmojiGuessGame.createGame()
+    
+   static func createGame() -> LogicBoard<String> {
+        let emojis = ["🐩", "🥤", "🚙"]
+    return LogicBoard<String>(numberOfPairOfCards: emojis.count) { pairIndex in
+           return emojis[pairIndex]
+        }
+    }
+    
+    //MARK: - Access to the model
+    
+    var cards: Array<LogicBoard<String>.Card> {
+        game.cards
+    }
+    
+    //MARK: - Intents
+    
+    func choose(card: LogicBoard<String>.Card) {
+        game.choose(card: card)
+        
+    }
+}
